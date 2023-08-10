ad6d58ea99043ef14a9d0c14ac9909cbf04fb6fd
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-08-10 11:40:40 +0100

    Add ssh-agency to fix magit

--- home/modules/spacemacs/spacemacs.el
+++ home/modules/spacemacs/spacemacs.el
@@ -79,7 +79,10 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages
+   '(
+     ssh-agency
+     )
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
