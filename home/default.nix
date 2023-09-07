04a0b372615890bcfe717c598589f72718b480c7
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-09-07 14:41:59 +0100

    Move fonts to home-manager and add Noto NF

--- home/default.nix
+++ home/default.nix
@@ -4,7 +4,16 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    font-awesome
+    liberation_ttf
+    (nerdfonts.override {fonts = ["SourceCodePro" "FiraCode" "CascadiaCode" "Noto"];})
+  ];
+in {
   imports = [
     ./modules/sway
     ./modules/waybar
@@ -24,9 +33,11 @@
     programs.home-manager.enable = true;
     systemd.user.startServices = "sd-switch";
 
-    home.packages = with pkgs; [
-      discord
-    ];
+    home.packages = with pkgs;
+      [
+        discord
+      ]
+      ++ fonts;
 
     programs.firefox.enable = true;
 
@@ -38,5 +49,7 @@
 
     xdg.userDirs.enable = true;
     services.mpd.enable = true;
+
+    fonts.fontconfig.enable = true;
   };
 }
