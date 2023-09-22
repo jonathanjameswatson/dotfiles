a664f8682584a6ebc62b34262a880cb61a2c7e49
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-09-22 14:57:14 +0100

    Add extra desktop apps

--- home/desktop/apps/extra/default.nix
+++ home/desktop/apps/extra/default.nix
@@ -7,5 +7,13 @@
   ...
 }: {
   home.packages = with pkgs; [
+    gnome.nautilus
+    gnome.eog
+    amberol
+    vlc
+    gnome.baobab
+    gnome.gnome-system-monitor
+    gnome.gnome-logs
+    gnome.gucharmap
   ];
 }
