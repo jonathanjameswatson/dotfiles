59c05bd4b7014e0a30ff3cf7e78cdef0cd2b7907
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2023-10-14 22:26:44 +0100

    Remove Nixos configuration comments

--- hosts/green/nixos/configuration.nix
+++ hosts/green/nixos/configuration.nix
@@ -8,11 +8,9 @@
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # boot.initrd.systemd.enable = true;
@@ -21,14 +19,8 @@
   # boot.kernelParams = ["quiet" "splash"];
   # boot.plymouth.enable = true;
 
-  networking.hostName = "green"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "green";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   jjw.locale.enable = true;
@@ -36,7 +28,6 @@
   console.earlySetup = true;
   console.font = "latarcyrheb-sun32";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jonathan = {
     isNormalUser = true;
     description = "Jonathan Watson";
@@ -45,38 +36,12 @@
     shell = pkgs.zsh;
   };
 
-  # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
     vim
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
