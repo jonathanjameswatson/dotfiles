70768c19d5842ef4561822228d1fd35a98a54bd1
Jonathan Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2024-01-06 00:24:29 +0000

    Configure Kanshi

--- home/desktop/wayland/default.nix
+++ home/desktop/wayland/default.nix
@@ -20,8 +20,37 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       wl-clipboard
-      kanshi
     ];
+
+    services.kanshi = {
+      enable = true;
+
+      profiles = {
+        undocked = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+            }
+          ];
+        };
+
+        docked = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "HDMI-A-1";
+              status = "enable";
+              mode = "2560x1440";
+              scale = 1.;
+            }
+          ];
+        };
+      };
+    };
 
     systemd.user.services.wl-clip-persist = {
       Unit = {
